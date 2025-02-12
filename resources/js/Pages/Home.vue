091e6e3d258baf3700913897561ace2bca0d0449
Yanik Kumar <believemaster@gmail.com>
2025-02-12 22:34:46 +0530

    Scroll preservation

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -2,11 +2,19 @@
 </script>
 
 <template>
+
     <Head :title="`| ${$page.component}`"></Head>
     <div class="p-4 m-4">
         <h1 class="inline p-2 text-5xl font-bold border-2 rounded-lg">
             Let's Learn InertiaJS!
         </h1>
         <p class="mt-5">Hello {{ $page.props.auth.user }}, welcome to your first Inertia app!</p>
+
+        <hr class="my-3" />
+
+        <Link class="mt-[600px] block bg-indigo-300 p-3 rounded-lg text-white text-center" href="/" preserve-scroll>Like
+        post at this position will not push it
+        to the top</Link>
+
     </div>
 </template>
