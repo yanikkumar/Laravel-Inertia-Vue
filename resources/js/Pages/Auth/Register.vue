55ff9424b96236951a331f607f0547deebd5f6d6
Yanik Kumar <yanikkumar@cdac.in>
2025-02-13 14:29:32 +0530

    Input field component

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -1,53 +1,57 @@
 <script setup>
-import { useForm } from '@inertiajs/vue3'
+import { useForm } from "@inertiajs/vue3";
+import TextInput from "../Component/TextInput.vue";
 
 const formData = useForm({
-    name: "",
-    email: "",
-    password: "",
-    password_confirmation: ""
+    name: null,
+    email: null,
+    password: null,
+    password_confirmation: null,
 });
 
 const submit = () => {
     // console.log(formData);
-    formData.post(route('register'), {
-        onError: () => formData.reset('password', 'password_confirmation')
+    formData.post(route("register"), {
+        onError: () => formData.reset("password", "password_confirmation"),
     });
-}
+};
 </script>
 
 <template>
-
     <Head :title="`| ${$page.component}`"></Head>
     <div class="p-4 m-4">
-        <h1 class="title">
-            Register
-        </h1>
-        <div class="w2/4 mx-auto">
+        <h1 class="title">Register</h1>
+        <div class="mx-auto w2/4">
             <form @submit.prevent="submit">
-                <div class="mb-6">
-                    <label for="name">Name</label>
-                    <input type="text" id="name" v-model="formData.name">
-                    <small class="text-red-500">{{ formData.errors.name }}</small>
-                </div>
-                <div class="mb-6">
-                    <label for="email">email</label>
-                    <input type="text" id="email" v-model="formData.email">
-                    <small class="text-red-500">{{ formData.errors.email }}</small>
-                </div>
-                <div class="mb-6">
-                    <label for="password">Password</label>
-                    <input type="password" id="password" v-model="formData.password">
-                    <small class="text-red-500">{{ formData.errors.password }}</small>
-                </div>
-                <div class="mb-6">
-                    <label for="password_confirmation">Confirm Password</label>
-                    <input type="password" id="password_confirmation" v-model="formData.password_confirmation">
-                    <small class="text-red-500">{{ formData.errors.password_confirmation }}</small>
-                </div>
+                <TextInput
+                    name="name"
+                    v-model="formData.name"
+                    :message="formData.errors.name"
+                />
+                <TextInput
+                    name="email"
+                    type="email"
+                    v-model="formData.email"
+                    :message="formData.errors.email"
+                />
+                <TextInput
+                    name="password"
+                    type="password"
+                    v-model="formData.password"
+                    :message="formData.errors.password"
+                />
+                <TextInput
+                    name="Confirm Password"
+                    type="password"
+                    v-model="formData.password_confirmation"
+                />
                 <div>
-                    <p class="text-slate-600 mb-2">Already a user? <a href="#" class="text-link">Login</a></p>
-                    <button class="primary-btn" :disabled="formData.processing">Register</button>
+                    <p class="mb-2 text-slate-600">
+                        Already a user? <a href="#" class="text-link">Login</a>
+                    </p>
+                    <button class="primary-btn" :disabled="formData.processing">
+                        Register
+                    </button>
                 </div>
             </form>
         </div>
